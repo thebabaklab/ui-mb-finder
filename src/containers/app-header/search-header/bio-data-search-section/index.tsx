4f18ce3d258b9bbac3d24a7d909df90ecd1bf296
Sebastian Rangel <sebokiabot@gmail.com>
2026-02-16 12:52:52 -0600

    Bug 39 - Bio Data error screen on search button click fixed

--- src/containers/app-header/search-header/bio-data-search-section/index.tsx
+++ src/containers/app-header/search-header/bio-data-search-section/index.tsx
@@ -29,7 +29,7 @@ export const BioDataSearchSection = () => {
       setLoading(true);
 
       const { data } = await axios.post(
-        `https://stage-api.mb-finder.org/api/v2/get-substances`,
+        `https://stage-api.mb-finder.org/api/v2/get-ceil-line-bio-data`,
         {
           ...search,
           currentPage,
