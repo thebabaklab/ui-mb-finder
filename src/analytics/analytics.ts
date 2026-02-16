8477a4dbbcd1314faa170112ef872cdd09e90769
Sebastian Rangel <sebokiabot@gmail.com>
2026-02-16 11:55:25 -0600

    GA4 - lint issues fixed

--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -1,6 +1,6 @@
-export const trackPage = (url: string) => {
+// export const trackPage = (url: string) => {
     // @ts-ignore
     // window.gtag('config', 'G-31LCYGK14G', {
     //     page_path: url,
     // });
-};
+// };
