95be3b05b3f98c1a00af389a9ba3bdedf6ecb114
Sebastian Rangel <sebokiabot@gmail.com>
2026-02-16 11:53:16 -0600

    GA4 - Analytics gtag implemented

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,23 @@
-import { createRootRoute, ErrorComponent, Outlet } from "@tanstack/react-router";
+import { createRootRoute, ErrorComponent, Outlet, useRouterState } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { trackPage } from "../analytics/analytics";
+
+const RootLayout = () => {
+  const pathname = useRouterState({
+    select: (s) => s.location.pathname,
+  });
+
+  useEffect(() => {
+    // console.log("Route:", pathname);
+    trackPage(pathname);
+  }, [pathname]);
+
+  return <Outlet />;
+}
 
 export const rootRoute = createRootRoute({
   component: () => <Outlet />,
+  // component: RootLayout,
   errorComponent: (() => ErrorComponent)(),
   notFoundComponent: () => <div>not found</div>,
 });
